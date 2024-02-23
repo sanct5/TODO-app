626d301a0e162d732cde79a7d93854a002d04768
sanct5 <130994767+sanct5@users.noreply.github.com>
2024-02-23 18:48:40 -0500

    Create Vite React App + TS + MUI + TailwindCSS

---
+++ ViteApp/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-500">
+      <Typography variant="h2" component="div" gutterBottom className="text-white">
+        ¡Bienvenido aquí se supone que estará la app!
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
---
+++ ViteApp/src/theme/globalThemes.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+export const mainTheme = createTheme({
+
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#3f51b5',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+});
