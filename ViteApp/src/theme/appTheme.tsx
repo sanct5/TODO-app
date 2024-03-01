efc3f71d7140cb445da909c34d53e43dc9459a4f
sanct5 <130994767+sanct5@users.noreply.github.com>
2024-03-01 16:37:40 -0500

    Responsive Sign-In component and structure folders

--- ViteApp/src/theme/appTheme.tsx
+++ ViteApp/src/theme/appTheme.tsx
@@ -1,9 +1,10 @@
+import { ReactNode } from 'react';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 
 import { mainTheme } from './globalThemes';
 
-export const AppTheme = ({ children }) => {
+export const AppTheme = ({ children }: { children: ReactNode }) => {
     return (
         <ThemeProvider theme={mainTheme}>
             <CssBaseline />
--- ViteApp/src/theme/globalThemes.tsx
+++ ViteApp/src/theme/globalThemes.tsx
@@ -1,14 +1,25 @@
 import { createTheme } from '@mui/material';
 
 export const mainTheme = createTheme({
-
     palette: {
         mode: 'light',
         primary: {
-            main: '#3f51b5',
+            main: '#6a0090',
         },
         secondary: {
-            main: '#f50057',
+            main: '#c1055a',
+        },
+        error: {
+            main: '#d32f2f',
+        },
+        warning: {
+            main: '#ed6c02',
+        },
+        info: {
+            main: '#0288d1',
+        },
+        success: {
+            main: '#2e7d32',
         },
     },
 });
