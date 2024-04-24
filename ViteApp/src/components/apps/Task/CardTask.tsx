f5489c364e5eed8dd198b9ee1a5f91bf893a3b39
Jhojan <147114192+jhojan16@users.noreply.github.com>
2024-04-24 00:03:37 -0500

    FEAT: detail task view

--- ViteApp/src/components/apps/Task/CardTask.tsx
+++ ViteApp/src/components/apps/Task/CardTask.tsx
@@ -29,7 +29,7 @@ function LinearProgressWithLabel(props: LinearProgressProps & { value: number })
     return (
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Box sx={{ width: '100%', pl: 2, pr: 1 }}>
-                <LinearProgress variant="determinate" {...props} sx={{ height: "7px" }} />
+                <LinearProgress variant="determinate" className="rounded-lg" {...props} sx={{ height: "10px" }} />
             </Box>
             <Box sx={{ pr: 2 }}>
                 <Typography variant="body2" color="text.secondary">{`${Math.round(
@@ -52,15 +52,15 @@ export default function ActionAreaCard({ id, title, endDate, steps }: ActionArea
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div" align="left" sx={{display:"flex", pl: 2, pr: 2 }}>
-                            <h2>{title}</h2>
+                            {title}
                         </Typography>
                         <Box sx={{ pl: 2 }}>
-                            <Typography variant="body2" color="text.primary">{"Progress"}</Typography>
+                            <Typography variant="body2" color="text.primary">Progreso</Typography>
                         </Box>
                         <LinearProgressWithLabel value={getProgress(steps)} />
                         <Typography gutterBottom variant="body2" component="div" sx={{ display: "flex", alignItems: 'center', pl: 2, pt: 1 }}>
                             <CalendarMonthIcon />
-                            <h2 className="pl-2">{endDate}</h2>
+                            {endDate}
                         </Typography>
                     </CardContent>
                 </CardActionArea>
--- ViteApp/src/components/apps/Task/TaskDetail.tsx
+++ ViteApp/src/components/apps/Task/TaskDetail.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import data from './data.json';
-import { Grid, Typography } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import logo from '../../../assets/Images/Logo.png';
 import { CardMedia } from "@mui/material";
 import Box from '@mui/material/Box';
@@ -13,12 +13,12 @@ import { useState } from 'react';
 
 
 const TaskDetail = () => {
-    const {id} = useParams();
+    const { id } = useParams();
     const task = data.tasks.find(task => task.id === parseInt(id));
     const [stepStatus, setStepStatus] = useState(
         task.steps.map((step) => step.status)
     );
-    
+
     // Función para renderizar la lista de pasos con íconos
     function renderSteps() {
         return task.steps.map((step, index) => (
@@ -27,11 +27,12 @@ const TaskDetail = () => {
                     control={
                         <Checkbox
                             checked={stepStatus[index]}
-                        onChange={(e) => {
+                        /*onChange={(e) => {
                             const newStatus = [...stepStatus];
                             newStatus[index] = e.target.checked;
                             setStepStatus(newStatus);
                         }}
+                        */
                         />
                     }
                     label={step.title} {...(stepStatus[index] ? { style: { textDecoration: 'line-through' } } : {})}
@@ -41,63 +42,43 @@ const TaskDetail = () => {
     }
 
     return (
-        <Grid container component="main" sx={{ minHeight: '10vh' }}>
-            <Grid sx={{ display: 'flex', alignItems: 'center' }}>
-                <Box
-                    sx={{
-                        my: 12,
-                        mx: 50,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center'
-                    }}
-                >
-                    <CardMedia
-                        component="img"
-                        style={{ height: "30vh" }}
-                        image={logo}
-                    />
-                    {task ? (
-                        <Grid container display={"flex"} flexDirection={"column"} justifyContent={"center"} pt={1} pl={2} pr={2}>
-                            <Typography variant="h4" fontWeight="bold">
-                                {task.title}
-                            </Typography>
-                            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} pt={1}>
-                                <Grid item xs={6}>
-                                    <Typography variant="body1" pl={1}>
-                                        <CalendarMonthIcon />
-                                        {task.startDate}
-                                    </Typography>
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <Typography variant="body1" pl={1}>
-                                        <DateRangeIcon />
-                                        {task.endDate}
-                                    </Typography>
-                                </Grid>
-                            </Grid>
-                            <Typography variant="h5" fontWeight="bold" pt={1}>
-                                Description
-                            </Typography>
-                            <Typography variant="body1" pt={1}>
-                                {task.description}
-                            </Typography>
-                            <Typography variant="h5" fontWeight="bold" pt={1}>
-                                Steps
-                            </Typography>
-                            <List dense={true}>
-                                {renderSteps()}
-                            </List>
-                        </Grid>
-                    ) : (
-                        <Typography variant="h1" fontWeight="bold">
-                            Task not found
+        <Container disableGutters className= "bg-white flex flex-col justify-center max-w-4xl rounded-lg">
+            <img src={logo} alt="Logo" className="rounded-lg" style={{ width: '100%', maxHeight: '250px', objectFit: "cover" }} />
+            <Box className="flex flex-col p-8">
+                <Box mb={2}>
+                    <Typography variant="h4" fontWeight="bold">
+                        {task.title}
+                    </Typography>
+                    <Box className="flex flex-col sm:flex-row">
+                        <Typography variant="body1">
+                            <CalendarMonthIcon color="primary"/>
+                            {task.startDate}
                         </Typography>
-                    )}
+                        <Typography variant="body1" className="ml-0 mt-2 sm:mt-0 sm:ml-5 ">
+                            <DateRangeIcon  color="primary"/>
+                            {task.endDate}
+                        </Typography>
+                    </Box>
                 </Box>
-            </Grid>
-        </Grid>
+                <Box mb={2}>
+                    <Typography variant="h5" fontWeight="bold" pt={1}>
+                        Descripción
+                    </Typography>
+                    <Typography variant="body1" pt={1}>
+                        {task.description}
+                    </Typography>
+                </Box>
+                <Box mb={2}>
+                    <Typography variant="h5" fontWeight="bold" pt={1}>
+                        Listado de pasos
+                    </Typography>
+                    <List dense={true}>
+                        {renderSteps()}
+                    </List>
+                </Box>
+            </Box>
+
+        </Container>
     );
 };
 export default TaskDetail;
