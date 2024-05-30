42f205c847d8e16c21a17ac63b076634055a1444
sanct5 <130994767+sanct5@users.noreply.github.com>
2024-05-29 20:42:50 -0500

    VIEW: delete task

--- ViteApp/src/components/apps/Task/TaskDetail.tsx
+++ ViteApp/src/components/apps/Task/TaskDetail.tsx
@@ -1,15 +1,8 @@
 import { useParams } from "react-router-dom";
-import { Container, Typography } from '@mui/material';
+import { Container, Dialog, Box, Typography, List, ListItem, CircularProgress, Button, IconButton, DialogContent, DialogContentText, DialogActions, Checkbox, FormControlLabel } from "@mui/material";
 import logo from '../../../assets/Images/Logo.png';
-import Box from '@mui/material/Box';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import DateRangeIcon from '@mui/icons-material/DateRange';
-import { List, ListItem, CircularProgress } from '@mui/material';
-import Checkbox from '@mui/material/Checkbox';
-import { FormControlLabel } from '@mui/material';
+import { Delete, ArrowBack, DateRange, CalendarMonth, Warning } from "@mui/icons-material";
 import { useState } from 'react';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import IconButton from '@mui/material/IconButton';
 import axios from 'axios';
 import { useEffect } from 'react';
 import { ManageTaskService } from '../../../api/tasks';
@@ -37,6 +30,8 @@ const TaskDetail = () => {
     const [loading, setLoading] = useState(true);
     const [checkedStates, setCheckedStates] = useState<Record<string, boolean>>({});
     const [updatingSteps, setUpdatingSteps] = useState<Record<string, boolean>>({});
+    const [openModal, setOpenModal] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
 
     useEffect(() => {
         const fetchTask = async () => {
@@ -98,11 +93,48 @@ const TaskDetail = () => {
         }
     }
 
+    const handleDelete = async () => {
+        setIsDeleting(true);
+        /*try {
+            const response = await axios.delete(`${ManageTaskService.baseUrl}${ManageTaskService.endpoints.deleteTask}/${id}`);
+            if (response.status !== 200) {
+                toast.error('Error al eliminar la tarea');
+            } else {
+                toast.success('Tarea eliminada');
+                setOpenModal(false);
+                window.history.back();
+            }
+        } catch (error) {
+            const res = (error as AxiosError).response?.status;
+            if (res === 500) {
+                toast.error('Error interno del servidor, inténtalo de nuevo más tarde');
+            } else {
+                toast.warn('Algo salió mal, no eres tu, somos nosotros, inténtalo de nuevo más tarde');
+            }
+        } finally {
+            setIsDeleting(false);
+        } */
+
+
+        // simular borrado exitoso
+        /*setTimeout(() => {
+            toast.success('Tarea eliminada');
+            setOpenModal(false);
+            window.history.back();
+        }, 2000); */
+
+        //Simular borrado fallido
+        setTimeout(() => {
+            toast.error('Error al eliminar la tarea');
+            setIsDeleting(false);
+        }, 2000);
+    }
+
     return (
         <Box className="flex flex-row">
             <Box className="hidden sm:block">
                 <IconButton onClick={() => window.history.back()}>
-                    <ArrowBackIcon color="primary" style={{ fontSize: 40 }} />
+                    <ArrowBack color="primary" style={{ fontSize: 40 }} />
                 </IconButton>
             </Box>
             <Container disableGutters className="bg-white flex flex-row justify-center max-w-3xl rounded-lg">
@@ -121,11 +153,11 @@ const TaskDetail = () => {
                                 </Typography>
                                 <Box className="flex flex-col sm:flex-row">
                                     <Typography variant="body1">
-                                        <CalendarMonthIcon color="primary" className="mr-2" />
+                                        <CalendarMonth color="primary" className="mr-2" />
                                         {format(task!.message.startDate, { date: "medium" })}
                                     </Typography>
                                     <Typography variant="body1" className="ml-0 mt-2 sm:mt-0 sm:ml-5 ">
-                                        <DateRangeIcon color="primary" className="mr-2" />
+                                        <DateRange color="primary" className="mr-2" />
                                         {format(task!.message.endDate, { date: "medium" })}
                                     </Typography>
                                 </Box>
@@ -164,9 +196,28 @@ const TaskDetail = () => {
                                     ))}
                                 </List>
                             </Box>
+                            <IconButton className="flex w-fit self-end">
+                                <Delete color="primary" onClick={() => setOpenModal(true)} fontSize="large" />
+                            </IconButton>
                         </Box>
                     </Box>)}
             </Container>
+            <Dialog open={openModal} onClose={() => setOpenModal(false)}>
+                <DialogContent>
+                    {!isDeleting ? (<Warning color="secondary" style={{ fontSize: 60, display: 'block', marginLeft: 'auto', marginRight: 'auto' }} />) : <CircularProgress style={{ fontSize: 50, display: 'block', marginLeft: 'auto', marginRight: 'auto' }} />}
+                    <DialogContentText align="center" fontSize={20} sx={{ marginTop: "25px" }}>
+                        ¿Estás seguro de que deseas eliminar la tarea: <b>{task?.message.title}</b>?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => setOpenModal(false)} color="secondary">
+                        Cancelar
+                    </Button>
+                    <Button onClick={handleDelete} color="primary">
+                        Eliminar
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Box>
     );
 };
